8f09d0c1cfb014c3776f6717413847861e25ca06
Beth Wilson <bethwilson777@gmail.com>
2022-08-29 15:15:21 -0600

    Add user repository Class code and Tests

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,20 @@
 class UserRepository {
+    constructor(data) {
+        this.data = data
+        // console.log(this.data)
+    }
+    findUserData(id){
+        const findUser = this.data.find(user => user.id === id)
+        return findUser
+    }
 
+    allUsersAverageSteps() {
+            const findAverage = this.data.reduce((avgSteps, user) => {
+                avgSteps += user.dailyStepGoal / this.data.length
+                return avgSteps
+            }, 0)
+            return findAverage
+        }
 }
 
 export default UserRepository;
