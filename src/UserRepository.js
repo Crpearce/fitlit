1618d194f39b0ba4aa873c867c7b08da15493375
Colby Pearce <crpearce892@gmail.com>
2022-09-01 21:12:40 -0600

    refactor weekly average function for error handling, update naming of keys in innerHTML

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -4,23 +4,24 @@ class UserRepository {
     this.friendNames = [];
   }
 
-    findUserData = (id) => this.data.find((user) => user.id === id);
+  findUserData = (id) => this.data.find((user) => user.id === id);
 
-    allUsersAverageSteps = () => this.data.reduce((avgSteps, user) => {
-        avgSteps += user.dailyStepGoal / this.data.length;
-        return Math.trunc(avgSteps);
-        }, 0);
+  allUsersAverageSteps = () =>
+    this.data.reduce((avgSteps, user) => {
+      avgSteps += user.dailyStepGoal / this.data.length;
+      return Math.trunc(avgSteps);
+    }, 0);
 
-    parseFriends(userId) {
-        this.data[userId -1].friends.forEach(friend => {
-            this.data.forEach(user => {
-                if (friend === user.id) {
-                    this.friendNames.push(' ' + user.name)
-                }
-            })
-        })
-        return this.friendNames
-    }
+  parseFriends = (userId) => {
+    this.data[userId - 1].friends.forEach((friend) => {
+      this.data.forEach((user) => {
+        if (friend === user.id) {
+          this.friendNames.push(" " + user.name);
+        }
+      });
+    });
+    return this.friendNames;
+  };
 }
 
 export default UserRepository;
--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,16 @@
 import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-// import userData from './data/users';
 import UserRepository from './UserRepository';
 import User from './User';
 import { fetchAll } from './apiCalls';
 import HydrationRepository from './HydrationRepository';
 import SleepRepository from './SleepRepository';
-//import dayjs from 'dayjs'
 
 // QUERYSELECTORS
 const dateInput = document.querySelector(".date-input");
 
 // GLOBAL VARIABLES
-
 let users;
 let userRepo;
 let hydroRepo;
@@ -24,29 +21,26 @@ let singleUser;
 const getFetch = () => {
     fetchAll()
     .then(data => {
-        console.log('data', data)
-        users = data[0].userData
-        sleep = data[1].sleepData
-        hydration = data[2].hydrationData
-        userRepo = new UserRepository(users)
-        singleUser = new User(users[getRandomUser()])
-        hydroRepo = new HydrationRepository(hydration)
-        sleepRepo = new SleepRepository(sleep)
-        welcomeUser()
-        displayUserInfo()
-        displayHydrationInfo()
-        displaySleepInfo()
-        // findDate()
-        // userRepo.findUserData(singleUser.id)
+        console.log('data', data);
+        users = data[0].userData;
+        sleep = data[1].sleepData;
+        hydration = data[2].hydrationData;
+        userRepo = new UserRepository(users);
+        singleUser = new User(users[getRandomUser()]);
+        hydroRepo = new HydrationRepository(hydration);
+        sleepRepo = new SleepRepository(sleep);
+        welcomeUser();
+        displayUserInfo();
+        displayHydrationInfo();
+        displaySleepInfo();
     })
 }
 
 //  EventListeners
+window.addEventListener('load', getFetch);
 sleepCalendarSection.addEventListener("click", handleButtons);
 
-const getRandomUser = () => {
-    return Math.floor(Math.random() * 49) + 1;
-};
+
 
 function handleButtons(event) {
     switch (event.target.className) {
@@ -62,15 +56,15 @@ function handleButtons(event) {
   }
 
 const welcomeUser = () => {
-    let greeting = document.querySelector('.welcome-customer')
-    greeting.innerText = `${singleUser.returnUserName()}`
-    let steps = document.querySelector('.daily-steps')
-    steps.innerText = `${singleUser.name.split(" ")[0]}'s Steps: ${singleUser.dailyStepGoal}
-    Group Average: ${userRepo.allUsersAverageSteps()}`
+    let greeting = document.querySelector('.welcome-customer');
+    greeting.innerText = `${singleUser.returnUserName()}`;
+    let steps = document.querySelector('.daily-steps');
+    steps.innerText = `${singleUser.name.split(" ")[0]}'s Steps: ${singleUser.dailyStepGoal};
+    Group Average: ${userRepo.allUsersAverageSteps()}`;
 }
 
 const displayUserInfo = () => {
-    let userCard = document.querySelector('.user-card')
+    let userCard = document.querySelector('.user-card');
     userCard.innerHTML = `<div>
     User Info
     <br>
@@ -80,7 +74,7 @@ const displayUserInfo = () => {
     <br>Stride Length: ${singleUser.strideLength}
     <br>Step Goal: ${singleUser.dailyStepGoal}
     <br>${singleUser.name.split(" ")[0]}'s Friends: ${userRepo.parseFriends(singleUser.id)}
-    </div>` 
+    </div>`;
 }
 
 const displayHydrationInfo = () => {
@@ -91,48 +85,61 @@ const displayHydrationInfo = () => {
     <br>${singleUser.name.split(" ")[0]}'s Average Ounces: ${hydroRepo.getUserAverageOunces(singleUser.id)}
     <br>Water Consumed Today: ${hydroRepo.ouncesConsumedByDate(singleUser.id)} oz.
     <br>Ounces Consumed This Week: ${hydroRepo.getWeeklyHydration(singleUser.id)}
-    </div>`
+    </div>`;
 }
 
 const findDate = () => {
-    let id = singleUser.id
-    let allSleepData = sleepRepo.sleepData.filter(user => user.userID === id)
-    const getDates = allSleepData.map(user => user.date).pop()
-    return getDates
+    let id = singleUser.id;
+    let allSleepData = sleepRepo.sleepData.filter(user => user.userID === id);
+    const getDates = allSleepData.map(user => user.date).pop();
+    return getDates;
 }
-{/* <br>Average Quality of Sleep: ${sleepRepo.getQualityOfSleep(singleUser.id)} */}
+
 const displaySleepInfo = () => {
     dateInput.value ='';
-    let sleepCard = document.querySelector('.sleep-card')
+    let sleepCard = document.querySelector('.sleep-card');
     sleepCard.innerHTML = `<div>
     Sleep Info
     <br>
+    <br>Daily Hours Slept: ${sleepRepo.sleepByDate(singleUser.id, 'hoursSlept')}
+    <br>Daily Quality of Sleep: ${sleepRepo.sleepByDate(singleUser.id, 'sleepQuality')}
     <br>Average Hours Slept: ${sleepRepo.getSleepAverage(singleUser.id, 'hoursSlept')}
     <br>Average Quality of Sleep: ${sleepRepo.getSleepAverage(singleUser.id, 'sleepQuality')}
-    <br>Daily Average Hours Slept: ${sleepRepo.sleepByDate(singleUser.id, 'hoursSlept')}
-    <br>Daily Average Quality of Sleep: ${sleepRepo.sleepByDate(singleUser.id, 'sleepQuality')}
     <br>Weekly Average of Hours Slept: ${sleepRepo.getWeeklySleep(singleUser.id, findDate(), 'hoursSlept')}
     <br>Weekly Average of Sleep Quality: ${sleepRepo.getWeeklySleep(singleUser.id, findDate(), 'sleepQuality')}
-    <br>Group Average of Sleep Quality: ${sleepRepo.allUsersAverageSleepQuality()}
-    </div>
-    `
+    <br>All Users Average of Sleep Quality: ${sleepRepo.allUsersAverageSleepQuality()}
+    </div>`
 }
 
 const updateSleepInfo = () => {
     let updateDate = dateInput.value.split('-').join('/');
-    let sleepCard = document.querySelector('.sleep-card')
+    let sleepCard = document.querySelector('.sleep-card');
+    dateInput.value === "" ? sleepCard.innerHTML = `<div>
+    Sleep Info
+    <br>
+    <br>Daily Hours Slept: ${sleepRepo.sleepByDate(singleUser.id, 'hoursSlept')}
+    <br>Daily Quality of Sleep: ${sleepRepo.sleepByDate(singleUser.id, 'sleepQuality', updateDate)}
+    <br>Average Hours Slept: ${sleepRepo.getSleepAverage(singleUser.id, 'hoursSlept')}
+    <br>Average Quality of Sleep: ${sleepRepo.getSleepAverage(singleUser.id, 'sleepQuality')}
+    <br>Weekly Average of Hours Slept: Please select a valid Date
+    <br>Weekly Average of Sleep Quality: Please select a Valid Date
+    <br>All Users Average of Sleep Quality: ${sleepRepo.allUsersAverageSleepQuality()}
+    </div>` :
     sleepCard.innerHTML = `<div>
     Sleep Info
     <br>
+    <br>Daily Hours Slept: ${sleepRepo.sleepByDate(singleUser.id, 'hoursSlept')}
+    <br>Daily Quality of Sleep: ${sleepRepo.sleepByDate(singleUser.id, 'sleepQuality', updateDate)}
     <br>Average Hours Slept: ${sleepRepo.getSleepAverage(singleUser.id, 'hoursSlept')}
     <br>Average Quality of Sleep: ${sleepRepo.getSleepAverage(singleUser.id, 'sleepQuality')}
-    <br>Daily Average Hours Slept: ${sleepRepo.sleepByDate(singleUser.id, 'hoursSlept')}
-    <br>Daily Average Quality of Sleep: ${sleepRepo.sleepByDate(singleUser.id, 'sleepQuality')}
     <br>Weekly Average of Hours Slept: ${sleepRepo.getWeeklySleep(singleUser.id, updateDate, 'hoursSlept')}
     <br>Weekly Average of Sleep Quality: ${sleepRepo.getWeeklySleep(singleUser.id, updateDate, 'sleepQuality')}
-    <br>Group Average of Sleep Quality: ${sleepRepo.allUsersAverageSleepQuality()}
-    </div>
-    `
+    <br>All Users Average of Sleep Quality: ${sleepRepo.allUsersAverageSleepQuality()}
+    </div>`;
 }
 
-window.addEventListener('load', getFetch)
+const getRandomUser = () => {
+    return Math.floor(Math.random() * 49) + 1;
+};
+
+  
